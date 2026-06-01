36a52c59288170ccb0f807593433d12074ac0eaa
Jennifer Páramo <jennifer.paramo10@gmail.com>
2026-06-01 12:31:14 -0600

    lanzamiento inicial del sitio personal

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("githubBtn").addEventListener("click", function(){
+        window.open("https://github.com/J-Paramo", "_blank");
+       // console.log("here");
+    });
+
+    document.getElementById("linkedinBtn").addEventListener("click", function(){
+        window.open("https://linkedin.com/in/jennifer-páramo", "_blank");
+    });
+    
+});
